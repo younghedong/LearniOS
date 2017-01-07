8fe27a8cb4a6f410fc7ba6069af9806187913567
hedong <hedong@hedongdeMacBook-Pro.local>
2017-01-07 15:44:30 +0800

    draw a rectangle at app start

--- Hypnosister/Hypnosister/AppDelegate.m
+++ Hypnosister/Hypnosister/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BNRHypnosisView.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    BNRHypnosisView *firstView = [[BNRHypnosisView alloc]
+                                  initWithFrame:firstFrame];
+    firstView.backgroundColor = [UIColor redColor];
+    
+    UIViewController *viewController = [[UIViewController alloc] init];
+    self.window.rootViewController = viewController;
+    
+    [self.window addSubview:firstView];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
