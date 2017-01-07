b2d08c3b0b70557c6a61104a0b763423f59d5880
hedong <hedong@hedongdeMacBook-Pro.local>
2017-01-07 15:24:05 +0800

    init project

---
+++ Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by hedong on 17/1/7.
+//  Copyright © 2017年 hedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/Hypnosister/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by hedong on 17/1/7.
+//  Copyright © 2017年 hedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
