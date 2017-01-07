784a328de1e88908b85562d3d3057b60452a4a05
hedong <hedong@hedongdeMacBook-Pro.local>
2017-01-07 15:49:21 +0800

    add BNRHyponsister.m and BNRHypnosisView.h

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by hedong on 17/1/7.
+//  Copyright © 2017年 hedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by hedong on 17/1/7.
+//  Copyright © 2017年 hedong. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
